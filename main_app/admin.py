b0465ebb7fb2a12254ebf3e1e6981b64e71dcf7a
LionMagnus <xinahoshinx@gmail.com>
2022-11-22 13:38:48 -0500

    CRUD update

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django_google_maps import widgets as map_widgets
+from django_google_maps import fields as map_fields
+from .models import Event
 
 # Register your models here.
+admin.site.register(Event)
+
+class RentalAdmin(admin.ModelAdmin):
+  formfield_overrides = {
+      map_fields.AddressField: {'widget': map_widgets.GoogleMapsAddressWidget},
+  }
