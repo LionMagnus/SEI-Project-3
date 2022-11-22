626497f0eb8cbda71d0d4394443fc0aecfbbdc2b
kelso-gh <soyouwontsingg@gmail.com>
2022-11-21 19:29:33 -0500

    model update one

--- main_app/models.py
+++ main_app/models.py
@@ -4,3 +4,13 @@ from datetime import date
 from django.contrib.auth.models import User
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=40)
+    age = models.IntegerField()
+    location = models.CharField(max_length=30)
+
+class Event(models.Model):
+    eventTitle = models.CharField(max_length=100)
+    description = models.TextField(max_length=2000)
+    date = models.DateField('Event Date')
+    evtLocation = models.CharField(max_length=50)
