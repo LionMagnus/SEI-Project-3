22a989b4f41b435ec55d43091f91ebc28a68c6ab
kelso-gh <soyouwontsingg@gmail.com>
2022-11-28 19:49:00 -0500

    recent update

--- main_app/admin.py
+++ main_app/admin.py
@@ -5,8 +5,3 @@ from .models import Event, Comment
 # Register your models here.
 admin.site.register(Event)
 admin.site.register(Comment)
-
-class RentalAdmin(admin.ModelAdmin):
-  formfield_overrides = {
-      map_fields.AddressField: {'widget': map_widgets.GoogleMapsAddressWidget},
-  }
