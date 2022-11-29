7006d38687c64c0aec75d37c9e8d9d758cf69e7b
LionMagnus <xinahoshinx@gmail.com>
2022-11-29 12:23:29 -0500

    funtionality update

--- main_app/models.py
+++ main_app/models.py
@@ -3,18 +3,13 @@ from django.urls import reverse
 from datetime import date
 from django.contrib.auth.models import User
 
-# Create your models here.
-# class User(models.Model):
-#   name = models.CharField(max_length=40)
-#   age = models.IntegerField()
-#   location = models.CharField(max_length=30)
-
 class Event(models.Model):
   eventTitle = models.CharField(max_length=100)
   description = models.TextField(max_length=2000)
   date = models.DateField('Event Date')
   evtLocation = models.CharField(max_length=50)
   organizer = models.ForeignKey(User, on_delete=models.CASCADE)
+  attendees = models.ManyToManyField(User, related_name="attendees")
   
   def __str__(self):
     return f'{self.eventTitle} ({self.id})'
@@ -26,9 +21,18 @@ class Comment(models.Model):
   comment = models.TextField(max_length=1000)
   event = models.ForeignKey(Event, on_delete=models.CASCADE)
   created_on = models.DateTimeField(auto_now_add=True)
-  active = models.BooleanField(default=False)
   user = models.ForeignKey(User, on_delete=models.CASCADE)
 
   class Meta:
     ordering = ['created_on']
 
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=12)
+
+class Photo(models.Model):
+    url = models.CharField(max_length=200)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Photo for event_id: {self.event_id} @{self.url}"
